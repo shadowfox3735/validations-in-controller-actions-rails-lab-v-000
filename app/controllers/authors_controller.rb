21ee2d4e9211b6d14bfc8cb720375b3f7362b5a3
shadowfox3735 <normanpan180@gmail.com>
2020-02-20 22:15:34 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -9,7 +9,20 @@ class AuthorsController < ApplicationController
   def create
     @author = Author.create!(author_params)
     
-    
+    <% if @author.errors.any? %>
+    <div id="error_explanation">
+      <h2>
+        <%= pluralize(@author.errors.count, "error") %>
+        prohibited this author from being saved:
+      </h2>
+   
+      <ul>
+      <% @author.errors.full_messages.each do |msg| %>
+        <li><%= msg %></li>
+      <% end %>
+      </ul>
+    </div>
+<% end %>
 
     redirect_to author_path(@author)
   end
