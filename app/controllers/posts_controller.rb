50d60f11101d92d92311de5809d1601a1588a5e9
shadowfox3735 <normanpan180@gmail.com>
2020-02-20 22:21:42 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,9 +8,8 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
+    if @post.update(post_params)
+      redirect_to post_path(@post)
   end
 
   private
